516d556cb8c47b79e725109189791d4a79736f72
chenguangjun <chen.gj@qq.com>
2021-12-03 12:27:34 +0800

    默认不打印调用栈信息

--- config/sqllog.php
+++ config/sqllog.php
@@ -3,5 +3,5 @@
 return [
     'slow' => env('SQLLOG_SLOW', true),
     'slow_time' => env('SQLLOG_SLOW_TIME', 0),
-    'debug_backtrace' => env('SQLLOG_DEBUG_BACKTRACE', true),
+    'debug_backtrace' => env('SQLLOG_DEBUG_BACKTRACE', false),
 ];
